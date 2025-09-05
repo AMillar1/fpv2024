7f2ee2d9fd7761212b1e71311841e0fa65407f1d
AMillar1 <40572479+AMillar1@users.noreply.github.com>
2025-09-05 10:06:54 -0400

    Create  my_lemmas.lean
    Initial setup

---
+++ my_lemmas.lean
@@ -0,0 +1,8 @@
+import Mathlib.Data.Nat.Prime.Basic
+import Mathlib.Data.Nat.Factorial.Basic
+import Mathlib.Tactic.Linarith
+
+/-- From Lecture 1: Proof that 0 + n = n using Lean. -/
+example (n : Nat) : 0 + n = n := by rfl
+
+
